5ffd8931217141307f54cb2bc647808d200c9317
MD ROKON MIA <89460952+MDROKONMIA@users.noreply.github.com>
2022-03-19 06:00:39 +0600

    initialize in projec

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './FrontEnd/components/Navbar';
 import Homepage from './FrontEnd/pages/Homepage/Homepage';
 import { BrowserRouter } from 'react-router-dom';
+import Footer from './FrontEnd/pages/Footer/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Homepage/>
+      <Homepage />
+      <Footer/>
     </BrowserRouter>
   );
 }
